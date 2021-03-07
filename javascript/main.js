dc6cd992bc56c89d5ceb3c62d6b892c9ec80a939
gallaodev <79752902+gallaodev@users.noreply.github.com>
2021-03-07 00:31:53 -0300

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,20 @@
 var nome = null
 var result = null
 var result = window.confirm("Voce quer entrar ?") //Botao ok e nao ok
-/*if (result === true){
+if (result === true){
     window.alert(`Voce escolheu entrar obrigado!`) 
+    var nome = window.prompt("Qual o seu nome?") //Botao para pegar o nome
+       if ( nome === 'gallao'){ 
+           window.alert('Voce tem permissao, e voce esta entrando no meu site, ' + nome + '!')
+           var num1 = Number.parseInt(window.prompt('Vamos somar numeros, aperte um numero'))
+           var num2 = Number(window.prompt('Aperte outro numero'))
+           var s = num1 + num2
+           window.alert(`A soma dos numeros ${num1} e ${num2} é: ${s} e seu nome tem ${nome.length} letras ${nome.        toUpperCase()} `)
+       }
+       else { window.alert("Voce nao tem permissao!")
+              window.location.href = ("http://pudim.com.br/")
+       }
 }
-else{window.alert(`Ok sem problemas!`) 
+else{window.alert(`Ok sem problemas!`)
      window.location.href = ("http://pudim.com.br/")
 } 
-*/ 
-var nome = window.prompt("Qual o seu nome?") //Botao para pegar o nome
-if ( nome === 'gallao'){ 
-    window.alert('Voce tem permissao, e voce esta entrando no meu site, ' + nome + '!') //Cria uma mensagem em um box na tela
-    var num1 = Number.parseInt(window.prompt('Vamos somar numeros, aperte um numero'))
-    var num2 = Number(window.prompt('Aperte outro numero'))
-    var s = num1 + num2
-    window.alert(`A soma dos numeros ${num1} e ${num2} é: ${s} e seu nome tem ${nome.length} letras ${nome. toUpperCase()} `)
-}
-else { window.alert("Voce nao tem permissao!")
-       window.location.href = ("http://pudim.com.br/")
-}
